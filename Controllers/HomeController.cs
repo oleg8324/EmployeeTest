cfaebb27041335d7be7d07523c92c201c48a22a9
oleg8324 <33290252+oleg8324@users.noreply.github.com>
2023-07-17 17:30:59 -0400

    added via ipload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using EmployeeTest.data;
+using EmployeeTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace EmployeeTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private EmployeeService _employeeService;
+
+        public HomeController(ILogger<HomeController> logger, EmployeeService employeeService)
+        {
+            _logger = logger;
+            _employeeService = employeeService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_employeeService.Employees);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        
+        [HttpPost]
+        public ActionResult Work(int id, IFormCollection form)
+        {
+            var employee = _employeeService.Employees[id]; // Get the employee with the given id
+            int days = int.Parse(form["days"]);
+            employee.Work(days);
+
+            return RedirectToAction("Index");
+        }
+
+        
+        [HttpPost]
+        public ActionResult TakeVacation(int id, IFormCollection form)
+        {
+            var employee = _employeeService.Employees[id]; // Get the employee with the given id
+            float days = float.Parse(form["days"]);
+            employee.TakeVacation(days);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ data/DataClasses.cs
@@ -0,0 +1,81 @@
+﻿namespace EmployeeTest.data
+{
+    
+    public abstract class Employee
+    {
+        private float _vacationDays = 0;
+        public int _workDays = 0;
+        public float VacationDays { get { return _vacationDays; } }
+        public int WorkDays { get { return _workDays; } }
+        public abstract float VacationDaysPerWorkYear { get; }
+
+        public void Work(int days)
+        {
+            if (days < 0 || days > 260)
+                throw new ArgumentException("Days worked must be a value between 0 and 260.");
+
+            _vacationDays += days * (VacationDaysPerWorkYear / 260);
+            _workDays += days;
+        }
+
+        public void TakeVacation(float days)
+        {
+            if (days < 0 || days > _vacationDays)
+                throw new ArgumentException("Cannot take more vacation than is available.");
+
+            _vacationDays -= days;
+        }
+    }
+
+    
+    public class HourlyEmployee : Employee
+    {
+        public override float VacationDaysPerWorkYear { get { return 10; } }
+    }
+
+    
+    public class SalariedEmployee : Employee
+    {
+        public override float VacationDaysPerWorkYear { get { return 15; } }
+    }
+
+    
+    public class Manager : Employee
+    {
+        public override float VacationDaysPerWorkYear { get { return 30; } }
+    }
+    
+    public static class EmployeeFactory
+    {
+        public static Employee CreateEmployee(string type)
+        {
+            switch (type)
+            {
+                case "Hourly":
+                    return new HourlyEmployee();
+                case "Salaried":
+                    return new SalariedEmployee();
+                case "Manager":
+                    return new Manager();
+                default:
+                    throw new ArgumentException("Invalid employee type.");
+            }
+        }
+    }
+    
+    public class EmployeeService
+    {
+        public List<Employee> Employees { get; private set; }
+
+        public EmployeeService()
+        {
+            Employees = new List<Employee>();
+            for (int i = 0; i < 10; i++)
+            {
+                Employees.Add(EmployeeFactory.CreateEmployee("Hourly"));
+                Employees.Add(EmployeeFactory.CreateEmployee("Salaried"));
+                Employees.Add(EmployeeFactory.CreateEmployee("Manager"));
+            }
+        }
+    }
+}
